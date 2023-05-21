efc0e3c4fe850204b9fcc3e28232225940d9dbc9
Mustafa Volkan <mustafa.vurulkan@gmail.com>
2023-05-22 01:06:33 +0300

    Add files via upload

---
+++ cluster-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+chmod 400 /var/lib/rabbitmq/.erlang.cookie
+
+HOSTNAME=`env hostname`
+echo "Starting RabbitMQ Server For host: " $HOSTNAME
+
+if [ -z "$JOIN_CLUSTER_HOST" ]; then
+    /usr/local/bin/docker-entrypoint.sh rabbitmq-server &
+    sleep 5
+    rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
+else
+    /usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
+    sleep 5
+    rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
+    rabbitmqctl stop_app
+    rabbitmqctl join_cluster rabbit@$JOIN_CLUSTER_HOST
+    rabbitmqctl start_app
+fi
+
+tail -f /var/log/rabbitmq/*.log
